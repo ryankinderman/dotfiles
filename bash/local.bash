90d1f88c2f891baf73faa5231f0cfad9ca305065
Ryan Kinderman <ryan@kinderman.net>
2012-06-17 08:47:16 -0500

    bash_local: Exiting decorated 'cd' with same status as original 'cd' if non-zero

--- bash/local.bash
+++ bash/local.bash
@@ -8,6 +8,9 @@ define_cd_with_bash_local() {
   local cmd=$1
   eval $cmd'_with_bash_local() {
     '$cmd'_without_bash_local "$@"
+    local exit_val=$?
+    [[ $exit_val != 0 ]] && return $exit_val
+
     local filename=.bash_local
     local script=${BASH_SOURCE[0]}
 
@@ -18,14 +21,14 @@ define_cd_with_bash_local() {
       else
         cat <<STR
 ===============================================================================
-  From: $script
+From: $script
 -------------------------------------------------------------------------------
-  $filename is present in the current directory, but not in the list of
-  allowed directories to auto-source from. For security, a directory must be
-  in the list of allowed directories before it'\''s auto-sourced. To add the
-  current directory to the list of allowed directories, run:
+$filename is present in the current directory, but not in the list of
+allowed directories to auto-source from. For security, a directory must be
+in the list of allowed directories before it'\''s auto-sourced. To add the
+current directory to the list of allowed directories, run:
 
-  echo "$PWD" >> $HOME/$filename.allowed
+echo "$PWD" >> $HOME/$filename.allowed
 ===============================================================================
 STR
       fi
