6bf872eaf0d11b790a727792ada6726f856f452a
Ryan Kinderman <ryan@kinderman.net>
2023-09-16 11:02:44 -0500

    vim: use per-project data dir for Java projects with nvim-jdtls

--- nvim/ftplugin/java.lua
+++ nvim/ftplugin/java.lua
@@ -1,5 +1,11 @@
+local project_name = string.gsub(string.sub(vim.fn.getcwd(), 2, -1), "/", "--")
+local workspace_dir = os.getenv('HOME') .. '/.local/share/nvim/nvim-jdtls/' .. project_name
+
 local config = {
-    cmd = {os.getenv('HOME') .. '/.local/share/nvim/mason/packages/jdtls/jdtls'},
+    cmd = {
+      os.getenv('HOME') .. '/.local/share/nvim/mason/packages/jdtls/jdtls',
+      '-data', workspace_dir,
+    },
     --root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw'}, { upward = true })[1]),
 }
 require('jdtls').start_or_attach(config)
