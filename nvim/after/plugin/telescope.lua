a3e1244ec948950abb9e49ab2eb88a41a1c0a8f0
Ryan Kinderman <ryan@kinderman.net>
2023-09-16 10:08:28 -0500

    vim: wrap lines with telescope

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,2 +1,9 @@
+-- Note: `brew install fd` to exclude files in list that are in `.gitignore`, e.g. `.class` files in Java
+require('telescope').setup({
+  defaults = {
+    wrap_results = true,
+  },
+})
+
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>t', builtin.find_files, {})
