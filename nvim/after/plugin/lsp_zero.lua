1f05002c7b125c40cc7f29535da462bb97daa5de
Ryan Kinderman <ryan@kinderman.net>
2023-09-18 18:01:19 -0500

    WIP

--- nvim/after/plugin/lsp_zero.lua
+++ nvim/after/plugin/lsp_zero.lua
@@ -1,24 +1,22 @@
-local lsp_zero = require('lsp-zero').preset({})
+local lsp_zero = require('lsp-zero.setup')
 
-local cmp = require('cmp')
-cmp.setup({
-  mapping = cmp.mapping.preset.insert({
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-d>'] = cmp.mapping.scroll_docs(4),
-  })
-})
-
-lsp_zero.on_attach(function(client, bufnr)
-  -- see :help lsp-zero-keybindings to learn the available actions
-  lsp_zero.default_keymaps({buffer = bufnr})
-
-  --local opts = {buffer = bufnr, remap = false}
-
-  --vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-end)
-
+--local cmp = require('cmp')
+--cmp.setup({
+--  mapping = cmp.mapping.preset.insert({
+--    ['<C-Space>'] = cmp.mapping.complete(),
+--    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+--    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+--    ['<C-d>'] = cmp.mapping.scroll_docs(4),
+--  })
+--})
+--
+--lsp_zero.on_attach(function(client, bufnr)
+--  -- see :help lsp-zero-keybindings to learn the available actions
+--  lsp_zero.default_keymaps({buffer = bufnr})
+--
+--  --local opts = {buffer = bufnr, remap = false}
+--
+--  --vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+--end)
+--
 require('lspconfig.ui.windows').default_options.border = 'single'
-
-lsp_zero.setup()
--- nvim/after/plugin/mason.lua
+++ nvim/after/plugin/mason.lua
@@ -1,22 +1,22 @@
-require("mason").setup()
-
-require("mason-lspconfig").setup()
-
-require("mason-lspconfig").setup_handlers {
-    -- The first entry (without a key) will be the default handler
-    -- and will be called for each installed server that doesn't have
-    -- a dedicated handler.
-    --function (server_name) -- default handler (optional)
-    --    require("lspconfig")[server_name].setup {}
-    --end,
-    -- Next, you can provide a dedicated handler for specific servers.
-    ["lua_ls"] = function ()
-      require('lspconfig').lua_ls.setup(
-        require('lsp-zero').nvim_lua_ls()
-      )
-    end,
-
-    ["jdtls"] = function ()
-      require('lspconfig').jdtls.setup({})
-    end,
-}
+--require("mason").setup()
+--
+--require("mason-lspconfig").setup()
+--
+--require("mason-lspconfig").setup_handlers {
+--    -- The first entry (without a key) will be the default handler
+--    -- and will be called for each installed server that doesn't have
+--    -- a dedicated handler.
+--    --function (server_name) -- default handler (optional)
+--    --    require("lspconfig")[server_name].setup {}
+--    --end,
+--    -- Next, you can provide a dedicated handler for specific servers.
+--    ["lua_ls"] = function ()
+--      require('lspconfig').lua_ls.setup(
+--        require('lsp-zero').nvim_lua_ls()
+--      )
+--    end,
+--
+--    ["jdtls"] = function ()
+--      require('lspconfig').jdtls.setup({})
+--    end,
+--}
