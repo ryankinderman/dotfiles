dd211e86e2f1df500dabc15c589a936c4f86e8c0
Ryan Kinderman <ryan@kinderman.net>
2023-09-20 21:05:45 -0500

    vim: use C-p and C-n for normal vim completion

--- nvim/after/plugin/lsp_zero.lua
+++ nvim/after/plugin/lsp_zero.lua
@@ -1,14 +1,9 @@
-local lsp_zero = require('lsp-zero').preset({})
+local lsp_zero = require('lsp-zero')
 
-local cmp = require('cmp')
-cmp.setup({
-  mapping = cmp.mapping.preset.insert({
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-d>'] = cmp.mapping.scroll_docs(4),
-  })
-})
+local cmp_config = require('cmp.config')
+local cmp_keymap = require('cmp.utils.keymap')
+cmp_config.global.mapping[cmp_keymap.normalize('<C-p>')] = nil
+cmp_config.global.mapping[cmp_keymap.normalize('<C-n>')] = nil
 
 lsp_zero.on_attach(function(client, bufnr)
   -- see :help lsp-zero-keybindings to learn the available actions
@@ -20,5 +15,3 @@ lsp_zero.on_attach(function(client, bufnr)
 end)
 
 require('lspconfig.ui.windows').default_options.border = 'single'
-
-lsp_zero.setup()
