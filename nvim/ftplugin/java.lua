8c891ffa482f0cea172a2bb8132447a8c18c01fe
Ryan Kinderman <ryan@kinderman.net>
2023-09-28 20:57:06 -0500

    vim: configure lspconfig's jdtls once when it's ready instead of for each buffer

--- nvim/ftplugin/java.lua
+++ nvim/ftplugin/java.lua
@@ -1,15 +1,3 @@
-local project_name = string.gsub(string.sub(vim.fn.getcwd(), 2, -1), "/", "--")
-local workspace_dir = os.getenv('HOME') .. '/.local/share/nvim/nvim-jdtls/' .. project_name
-
-local config = {
-    cmd = {
-      os.getenv('HOME') .. '/.local/share/nvim/mason/packages/jdtls/jdtls',
-      '-data', workspace_dir,
-    },
-    --root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw'}, { upward = true })[1]),
-}
-require('jdtls').start_or_attach(config)
-
 vim.bo.shiftwidth = 4
 vim.bo.tabstop = 4
 vim.bo.softtabstop = 4
