3ae9c68435eaf689117259f54729c3e4b9f7735e
Ryan Kinderman <ryan@kinderman.net>
2023-10-12 09:32:35 -0500

    nvim: nvim-jdtls seems to need to be initialized per-buffer

--- nvim/after/plugin/mason.lua
+++ nvim/after/plugin/mason.lua
@@ -15,27 +15,4 @@ require("mason-lspconfig").setup_handlers {
         require('lsp-zero').nvim_lua_ls()
       )
     end,
-
-    ["jdtls"] = function ()
-      local project_name = string.gsub(string.sub(vim.fn.getcwd(), 2, -1), "/", "--")
-      local util = require 'lspconfig.util'
-      local workspace_dir = util.path.join('nvim', 'nvim-jdtls-data', project_name)
-      local env = {
-        HOME = vim.loop.os_homedir(),
-        XDG_CACHE_HOME = os.getenv 'XDG_CACHE_HOME',
-      }
-      local get_cache_dir = function()
-        return env.XDG_CACHE_HOME and env.XDG_CACHE_HOME or util.path.join(env.HOME, '.cache')
-      end
-
-      -- See `nvim-lspconfig/lua/lspconfig/server_configurations/jdtls.lua` for more options
-      require('lspconfig').jdtls.setup({
-          cmd = {
-            'jdtls',
-            '-configuration', util.path.join(get_cache_dir(), 'config'),
-            '-data', util.path.join(get_cache_dir(), workspace_dir),
-          },
-          --root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw'}, { upward = true })[1]),
-      })
-    end,
 }
--- nvim/ftplugin/java.lua
+++ nvim/ftplugin/java.lua
@@ -3,3 +3,33 @@ vim.bo.tabstop = 4
 vim.bo.softtabstop = 4
 vim.bo.expandtab = true
 vim.bo.cindent = false
+
+local project_name = string.gsub(string.sub(vim.fn.getcwd(), 2, -1), "/", "--")
+local util = require 'lspconfig.util'
+local workspace_dir = util.path.join('jdtls-data', project_name)
+local env = {
+  HOME = vim.loop.os_homedir(),
+  XDG_CACHE_HOME = os.getenv 'XDG_CACHE_HOME',
+}
+local get_cache_dir = function()
+  return util.path.join(
+    (env.XDG_CACHE_HOME and env.XDG_CACHE_HOME or util.path.join(env.HOME, '.cache')),
+    'nvim')
+end
+
+-- See `nvim-lspconfig/lua/lspconfig/server_configurations/jdtls.lua` for more options
+local config = {
+  cmd = {
+    'jdtls',
+    '-configuration', util.path.join(get_cache_dir(), 'jdtls-config'),
+    '-data', util.path.join(get_cache_dir(), workspace_dir),
+  },
+  --root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw'}, { upward = true })[1]),
+}
+
+local jdtls = require('jdtls')
+jdtls.start_or_attach(config)
+
+vim.keymap.set('n', '<A-o>', function() jdtls.organize_imports() end, {
+  ['buffer'] = true, ['noremap'] = true,
+})
