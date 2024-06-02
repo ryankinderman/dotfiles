5a5336fe71edff479a288003e4a225e4b3a3e31a
Ryan Kinderman <ryan@kinderman.net>
2024-06-02 08:47:46 -0500

    nvim: Only configure changes to rose-pine defaults

--- nvim/after/plugin/rose-pine.lua
+++ nvim/after/plugin/rose-pine.lua
@@ -1,58 +1,10 @@
-require('rose-pine').setup({
-  --- @usage 'auto'|'main'|'moon'|'dawn'
-  variant = 'auto',
-  --- @usage 'main'|'moon'|'dawn'
-  dark_variant = 'main',
-  bold_vert_split = false,
-  dim_nc_background = false,
-  disable_background = false,
-  disable_float_background = true,
-  disable_italics = false,
+require('rose-pine').setup(
+{
+  dim_inactive_windows = true,
 
-  --- @usage string hex value or named color from rosepinetheme.com/palette
-  groups = {
-    background = 'none',
-    background_nc = '_experimental_nc',
-    panel = 'surface',
-    panel_nc = 'base',
-    border = 'highlight_med',
-    comment = 'muted',
-    link = 'iris',
-    punctuation = 'subtle',
-
-    error = 'love',
-    hint = 'iris',
-    info = 'foam',
-    warn = 'gold',
-
-    headings = {
-      h1 = 'iris',
-      h2 = 'foam',
-      h3 = 'rose',
-      h4 = 'gold',
-      h5 = 'pine',
-      h6 = 'foam',
-    }
-    -- or set all headings at once
-    -- headings = 'subtle'
-  },
-
-  -- Change specific vim highlight groups
-  -- https://github.com/rose-pine/neovim/wiki/Recipes
   highlight_groups = {
-    ColorColumn = { bg = 'rose' },
-
-    -- Blend colours against the "base" background
-    CursorLine = { bg = 'foam', blend = 10 },
-    StatusLine = { fg = 'love', bg = 'love', blend = 10 },
-
-    -- By default each group adds to the existing config.
-    -- If you only want to set what is written in this config exactly,
-    -- you can set the inherit option:
-    Search = { bg = 'highlight_med', inherit = false },
-
     Folded = { fg = 'subtle' }
-  }
+  },
 })
 
 -- Set colorscheme after options
