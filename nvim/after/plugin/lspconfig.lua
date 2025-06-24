69d2ddcaf13a334f05ee02ac96705d4f03ec14e7
Ryan Kinderman <ryan@kinderman.net>
2025-06-24 17:39:28 -0500

    Configure pkl LSP

--- nvim/after/plugin/lspconfig.lua
+++ nvim/after/plugin/lspconfig.lua
@@ -1,1 +1,8 @@
 require('lspconfig.ui.windows').default_options.border = 'single'
+
+local pkl_lsp_jar = vim.fn.glob(vim.fn.stdpath('data') .. "/mason/packages/pkl-lsp" .. "/*.jar")
+
+vim.g.pkl_neovim = {
+  start_command = {
+    os.getenv('JAVA24_HOME') .. "/bin/java", "-jar", pkl_lsp_jar }
+}
